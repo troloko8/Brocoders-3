72a33efac20d776aab20fc1faee66811a7f6bf5a
troloko <troloko8@gmail.com>
2019-09-03 18:48:15 +0300

    Updates

---
+++ precache-manifest.f95bcd2dd6efb93f74ca8ed9acabfc40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8161700e33473c9b904cb4992a5c8f2",
+    "url": "/Brocoders-3/index.html"
+  },
+  {
+    "revision": "e292359faa6213b2c723",
+    "url": "/Brocoders-3/static/css/main.8d8cd586.chunk.css"
+  },
+  {
+    "revision": "c2c167d3a59072ade3c1",
+    "url": "/Brocoders-3/static/js/2.92c07f48.chunk.js"
+  },
+  {
+    "revision": "e292359faa6213b2c723",
+    "url": "/Brocoders-3/static/js/main.46a7a15e.chunk.js"
+  },
+  {
+    "revision": "329865b3ac004efecb6c",
+    "url": "/Brocoders-3/static/js/runtime~main.3e818013.js"
+  }
+]);
